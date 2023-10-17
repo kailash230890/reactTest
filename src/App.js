5e8d7847ab70133cef06f115b5e053a8563bcfdd
Kailash <kailash230890@gmail.com>
2023-10-17 17:14:49 +0530

    added stopwatch files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Stopwatch from "./Stopwatch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h3>REACT APPLICATION </h3>
         <p>Created by: Kailash Software Engineer</p>
+        <Stopwatch />
       </header>
     </div>
   );
